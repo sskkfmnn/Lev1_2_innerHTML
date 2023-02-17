3608ff00f872352f768fd2fb41e79ad5f49edb0b
sskkfmnn <sskkfmnn@gmail.com>
2023-02-17 14:34:12 +0100

    Lev1_2

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+console.log ("");
+
+const info = document.getElementById("info");
+info.innerHTML = ("<h1>Hello World</h1>");
+info.innerHTML += ("<h2>How are you?</h2>");
+
+const container = document.getElementById("container");
+// container.innerHTML = ("start of the element");
+document.write ("end of the element");
